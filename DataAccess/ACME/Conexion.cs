72ecf1db29a47364368e121020d7ebcd77a43e82
Kevin22077 <alfaro.kevin0501@gmail.com>
2024-04-20 23:18:02 -0600

    Agregar archivos de proyecto.

---
+++ DataAccess/ACME/Conexion.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Data.SqlClient;
+
+namespace DataAccess.ACME
+{
+    public class Conexion
+    {
+        private readonly string? _cadenaConexion;
+        public Conexion()
+        {
+            string? cadenaConexion;
+            // Obtener la cadena de conexion desde variable de entorno
+            cadenaConexion = Environment.GetEnvironmentVariable("SQLServerXE");
+            _cadenaConexion = cadenaConexion;
+        }
+
+        public SqlConnection Conectar()
+        {
+            SqlConnection sqlConn;
+            // Instanciar la conexion utilizando la candena de conexion obtenida
+            sqlConn = new SqlConnection(_cadenaConexion);
+            return sqlConn;
+        }
+    }
+    
+}
---
+++ XperTek_ACME/Controllers/EmpresaController.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Models.ACME;
+using Services.ACME;
+
+namespace XperTek_ACME.Controllers
+{
+    public class EmpresaController : Controller
+    {
+        [HttpGet]
+        public IActionResult Index()
+        {
+            // Listar las empresas
+            EmpresaService empresaService = new EmpresaService();
+            List<EmpresaEntidad>? listaEmpresaEntidad;
+            
+            listaEmpresaEntidad = empresaService.Listar();
+            return View(listaEmpresaEntidad);
+        }
+    }
+}
